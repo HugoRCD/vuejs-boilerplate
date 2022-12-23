ceb262412a2da29d8d7cb0a3bf1366174535e212
Hugo Richard <hrichard206@gmail.com>
2022-12-23 19:41:08 +0100

    Start switching to TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,31 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    fontSize: {
+      sm: ["0.875rem", { lineHeight: "1.25rem", letterSpacing: "-0.03em" }],
+      base: ["1rem", { lineHeight: "1.5rem", letterSpacing: "-0.03em" }],
+      lg: ["1.125rem", { lineHeight: "1.75rem", letterSpacing: "-0.03em" }],
+      xl: ["1.25rem", { lineHeight: "1.75rem", letterSpacing: "-0.03em" }],
+      "2xl": ["1.5rem", { lineHeight: "2rem", letterSpacing: "-0.03em" }],
+    },
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+    },
+    screens: {
+      mobile: "320px",
+      tablet: "960px",
+      desktop: "1280px",
+    },
+    colors: {
+      transparent: "transparent",
+      white: "#ffffff",
+      black: "#1f1f1f",
+      grass: "#10B981",
+    },
+    boxShadow: {
+      sm: "0 1px 2px 0 rgba(0, 0, 0, 0.05)",
+      l: "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
+    },
     extend: {},
   },
   plugins: [],
